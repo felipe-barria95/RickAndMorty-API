fde02d188f9fb88573ea6b7672a90bbe071665bb
felipe-barria95 <frbarria@uc.cl>
2020-03-27 23:56:54 -0300

    commit

--- tarea_1/settings.py
+++ tarea_1/settings.py
@@ -58,6 +58,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'tarea_1.urls'
 
 TEMPLATES = [
